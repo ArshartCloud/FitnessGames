d3cedbe3dac2fc7e06167b6189c682145f942d24
Arshart Cloud <askheart1@gmail.com>
2019-03-23 18:31:11 -0500

    Update ReclaimWall.cs

--- FitnessGames/Assets/Scripts/ReclaimWall.cs
+++ FitnessGames/Assets/Scripts/ReclaimWall.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReclaimWall : Interactive {
+public class ReclaimWall : Affect {
+    protected override void OnTriggerExit(Collider trigger)
+    {
+        // Update all the states
+        OnTriggerUpdate();
+        // Avoid self triggering
+        if (trigger.gameObject != gameObject)
+        {
+            // Avoid non-interactives unless not required
+            if (trigger.gameObject.GetComponent<Interactive>() != null || interactivesOnly == false)
+            {
+                // Update the current state value
+                triggerExited = true;
+                // Keep track of the current trigger
+                exitedTriggers.Add(trigger);
+            }
+        }
+        FlyingObject fo = trigger.GetComponent<FlyingObject>();
+        if (fo != null)
+        {
+            if (fo.state == FlyingObject.FlyingObjectState.Touched)
+            {
+                GameManager.instance.ChangeScore(-fo.score);
+            }
+            fo.Reclaim();
+        }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    }
+
 }
