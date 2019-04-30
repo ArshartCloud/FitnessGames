bd8ce8ed4e2d5807806b8897580a0d8eae015bb8
dhwaniraval <Kdvs#379>
2019-04-29 23:33:22 -0500

    Light Saber As Vive Contorller
    - Added two new scenes as New_Twist and New_ArmRaise
    - Added light saber as controller
    - Added countdown animation
    - Added new fonts

---
+++ FitnessGames/Assets/Scripts/countDown.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class countDown : MonoBehaviour {
+
+    float currentTime = 0f;
+    float startingTime = 5f;
+
+    [SerializeField] TextMeshPro countdownText;
+
+    // Use this for initialization
+    void Start()
+    {
+        countdownText = GetComponent<TextMeshPro>();
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = Mathf.RoundToInt(currentTime).ToString();
+
+        // countdownText.text = currentTime.ToString("0");
+
+        if(currentTime <= 0)
+        {
+            countdownText.text = "GO!";
+
+            // Start the game here
+        }
+    }
+}
