66ee195f8d52ae13433da7564261da394ae2c539
Di Jin <332548128@qq.com>
2019-05-01 11:09:20 -0500

    -

---
+++ FitnessGames/Assets/Resources/Animations/ani 1/ChangeTo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Change(GameObject other)
+    {
+        other.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
